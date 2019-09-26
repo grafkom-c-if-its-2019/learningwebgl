fb2e7dfb9b43979321b1a6f56f901611d6403158
Hadziq 哈明飛 <hadziq@gmail.com>
2019-09-26 08:11:55 +0700

    Menggambar segitiga

--- index.js
+++ index.js
@@ -13,12 +13,46 @@
 
     gl.useProgram(program);
 
+    var triangleVertices = [
+      // x, y,      r, g, b
+      0.0, 0.5,     1.0, 1.0, 0.0,
+      -0.5, -0.5,   0.7, 0.0, 1.0,
+      0.5, -0.5,    0.1, 1.0, 0.6
+    ];
+
+    // Link antara CPU Memory dengan GPU Memory
+    var triangleVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+
+    // Link untuk attribute
+    var vPosition = gl.getAttribLocation(program, 'vPosition');
+    var vColor = gl.getAttribLocation(program, 'vColor');
+    gl.vertexAttribPointer(
+      vPosition,  // variabel yang memegang posisi attribute di shader
+      2,          // jumlah elemen per atribut
+      gl.FLOAT,   // tipe data atribut
+      gl.FALSE, 
+      5 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap vertex 
+      0                                   // offset dari posisi elemen di array
+    );
+    gl.vertexAttribPointer(
+      vColor,
+      3,
+      gl.FLOAT,
+      gl.FALSE,
+      5 * Float32Array.BYTES_PER_ELEMENT,
+      2 * Float32Array.BYTES_PER_ELEMENT
+    );
+    gl.enableVertexAttribArray(vPosition);
+    gl.enableVertexAttribArray(vColor);
+
     // Bersihkan layar jadi hitam
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     // Bersihkan buffernya canvas
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
   }
 })();
