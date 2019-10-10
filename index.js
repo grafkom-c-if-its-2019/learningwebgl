5cf02a702908920fc252a2fa833273b8b6b5f79d
Hadziq Fabroyir 哈明飛 <hadziq@gmail.com>
2019-10-10 08:30:22 +0700

    Berhasil gambar kubus

--- index.js
+++ index.js
@@ -13,74 +13,92 @@
 
     gl.useProgram(program);
 
-    var triangleVertices = [
-      // x, y,      r, g, b
-      0.0, 0.5,     1.0, 1.0, 0.0,
-      -0.5, -0.5,   0.7, 0.0, 1.0,
-      0.5, -0.5,    0.1, 1.0, 0.6
+    var cubeVertices = [];
+
+    // Posisi 8 titik kubus
+    var cubePoints = [
+      [ -0.5, -0.5,  0.5 ],
+      [ -0.5,  0.5,  0.5 ],
+      [  0.5,  0.5,  0.5 ],
+      [  0.5, -0.5,  0.5 ],
+      [ -0.5, -0.5, -0.5 ],
+      [ -0.5,  0.5, -0.5 ],
+      [  0.5,  0.5, -0.5 ],
+      [  0.5, -0.5, -0.5 ]
     ];
+    var cubeColors = [
+      [],
+      [ 1.0, 0.0, 0.0 ], // merah
+      [ 1.0, 1.0, 0.0 ], // kuning
+      [ 0.0, 1.0, 0.0 ], // hijau
+      [ 0.0, 0.0, 1.0 ], // biru
+      [ 1.0, 1.0, 1.0 ], // putih
+      [ 1.0, 0.5, 0.0 ], // orange
+      []
+    ];
+    function quad(a, b, c, d) {
+      var indices = [a, b, c, a, c, d];
+      for (var i = 0; i < indices.length; i++) {
+        for (var j = 0; j < 3; j++) {
+          cubeVertices.push(cubePoints[indices[i]][j]);
+        }
+        for (var j = 0; j < 3; j++) {
+          cubeVertices.push(cubeColors[a][j]);
+        }
+      }
+    }
+    quad(1, 0, 3, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
+    quad(6, 5, 1, 2);
 
     // Link antara CPU Memory dengan GPU Memory
-    var triangleVertexBufferObject = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+    var cubeVBO = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVBO);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeVertices), gl.STATIC_DRAW);
 
     // Link untuk attribute
     var vPosition = gl.getAttribLocation(program, 'vPosition');
     var vColor = gl.getAttribLocation(program, 'vColor');
     gl.vertexAttribPointer(
       vPosition,  // variabel yang memegang posisi attribute di shader
-      2,          // jumlah elemen per atribut
+      3,          // jumlah elemen per atribut
       gl.FLOAT,   // tipe data atribut
-      gl.FALSE, 
-      5 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap vertex 
+      false, 
+      6 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap vertex 
       0                                   // offset dari posisi elemen di array
     );
     gl.vertexAttribPointer(
       vColor,
       3,
       gl.FLOAT,
-      gl.FALSE,
-      5 * Float32Array.BYTES_PER_ELEMENT,
-      2 * Float32Array.BYTES_PER_ELEMENT
+      false,
+      6 * Float32Array.BYTES_PER_ELEMENT,
+      3 * Float32Array.BYTES_PER_ELEMENT
     );
     gl.enableVertexAttribArray(vPosition);
     gl.enableVertexAttribArray(vColor);
 
-    var translation = gl.getUniformLocation(program, 'translation');
-    var translationVector = [0.0, -0.5, 0.0];
-    gl.uniform3fv(translation, translationVector);
-
     var thetaLocation = gl.getUniformLocation(program, 'theta');
     var theta = 0.0;
 
-    var scaleXLocation = gl.getUniformLocation(program, 'scaleX');
-    var scaleYLocation = gl.getUniformLocation(program, 'scaleY');
-    var scaleX = 1.0;
-    var scaleY = 1.0;
-    var melebar = 1;
-
     function render() {
-      // Bersihkan layar jadi hitam
-      gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  
       // Bersihkan buffernya canvas
-      gl.clear(gl.COLOR_BUFFER_BIT);
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-      /*
       theta += 0.01;
       gl.uniform1f(thetaLocation, theta);
-      */
-
-      if (scaleX >= 1) melebar = -1;
-      else if (scaleX <= -1) melebar = 1;
-      scaleX += 0.01 * melebar;
-      gl.uniform1f(scaleXLocation, scaleX);
-      gl.uniform1f(scaleYLocation, scaleY);
   
-      gl.drawArrays(gl.TRIANGLES, 0, 3);
+      gl.drawArrays(gl.TRIANGLES, 0, 36);
       requestAnimationFrame(render);
     }
+
+    // Bersihkan layar jadi hitam
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    gl.enable(gl.DEPTH_TEST);
     render();
   }
 })();
